cf1f75257856682c5ec4af456183748935784bce
Adrián Erik Clarenbeek Sánchez <adrian.clarenbeek@omegapoint.se>
2024-03-09 00:16:26 +0100

    asd

--- src/test/java/SecurityConfiguration.java
+++ src/test/java/SecurityConfiguration.java
@@ -1,5 +1,3 @@
-package org.example;
-
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
