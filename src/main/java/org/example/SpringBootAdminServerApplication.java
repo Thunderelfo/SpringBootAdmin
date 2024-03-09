ee1864bebfb9c3d00ca21407f977ee51494ccb99
Adrián Erik Clarenbeek Sánchez <adrian.clarenbeek@omegapoint.se>
2024-03-09 23:29:09 +0100

    working server (ish)

--- src/main/java/org/example/SpringBootAdminServerApplication.java
+++ src/main/java/org/example/SpringBootAdminServerApplication.java
@@ -3,10 +3,13 @@ package org.example;
 import de.codecentric.boot.admin.server.config.AdminServerHazelcastAutoConfiguration;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Import;
 
 @EnableAdminServer
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SpringBootAdminServerApplication{
 
     public static void main(String[] args) {
